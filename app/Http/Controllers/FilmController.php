21ae62ffcd0ad3c8f8e18fc6d0713fcc288dd186
Azkaaja <bolbagantunahan22@gmail.com>
2023-03-24 09:44:41 +0700

    add create film

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Film as ModelsFilm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Models\Film;
 
 class FilmController extends Controller
@@ -22,5 +23,18 @@ class FilmController extends Controller
     public function show2($id){
         $post = ModelsFilm::findOrFail($id);
         return new PostDetailResource($post);
+    }
+    public function store(Request $request){
+        $request -> validate([
+            'title' => 'required|max:255',
+            'main_genre' => 'required',
+            'synopsis'=>'required'
+        ]);
+
+        // return response()->json('sudah dapat digunakan');
+        $request['author'] = Auth::user()->id;
+
+        $post = ModelsFilm::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,18 @@ use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts',[FilmController::class,'index'])->middleware('auth:sanctum');
-Route::get('/posts/{id}',[FilmController::class,'show'])->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::get('/posts',[FilmController::class,'index']);
+    Route::get('/posts/{id}',[FilmController::class,'show']);
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+    Route::get('/me', [AuthenticationController::class, 'me']);
+    Route::post('/posts', [FilmController::class, 'store']);
+});
+
+
 Route::get('/posts2/{id}', [FilmController::class, 'show2']);
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
+
 
 
 
