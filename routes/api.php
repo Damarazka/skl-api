6425eaf0cfb058c438f8b7d3d0fe0364f2b34db7
Azkaaja <bolbagantunahan22@gmail.com>
2023-03-24 09:03:44 +0700

    create login whit token

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/posts',[FilmController::class,'index']);
 Route::get('/posts/{id}',[FilmController::class,'show']);
 Route::get('/posts2/{id}', [FilmController::class, 'show2']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
 
